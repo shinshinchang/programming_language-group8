dc3722c48d848038efcc779821ebb25792e98353
Adrian-Kao <adrian950125@gmail.com>
2025-05-13 09:47:44 +0800

    Update CustomerDetailPanel.java

--- NCCUMarcket/NCCUMarcket+/Layout/CustomerDetailPanel.java
+++ NCCUMarcket/NCCUMarcket+/Layout/CustomerDetailPanel.java
@@ -1,6 +1,16 @@
 package Layout;
 
 import java.awt.event.*;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.Map;
+import java.net.URL;
+import java.lang.reflect.Type;
+
+import javax.swing.JOptionPane;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class CustomerDetailPanel extends VendorEditPanel {
     public CustomerDetailPanel(MainFrame frame) {
@@ -28,10 +38,39 @@ public class CustomerDetailPanel extends VendorEditPanel {
         
     }
 
-    public void refresh(String id) {
-        // 從資料庫抓資料把標籤的資料更新，當browserpanel的商家按鈕按下，會callout
-        // frame的refresh()，然後frame會call這個refresh，這裡在更新顯示標籤就好
-        // 簡單來說商家按鈕按下這裡就顯示
+   public void refresh(String id) {
+        try {
+            URL url = new URL("https://nccu-market-default-rtdb.asia-southeast1.firebasedatabase.app/vendors/" + id + ".json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
 
+            InputStreamReader reader = new InputStreamReader(conn.getInputStream());
+            Gson gson = new Gson();
+            Type type = new TypeToken<Map<String, Object>>(){}.getType();
+            Map<String, Object> vendor = gson.fromJson(reader, type);
+            reader.close();
+
+            stallIdField.setText(id);
+            nameField.setText((String) vendor.get("name"));
+            contactField.setText((String) vendor.get("contact_info"));
+            promoArea.setText((String) vendor.get("description"));
+
+            // 標籤處理
+            String tags = (String) vendor.get("tags");
+            eatTag.setSelected(tags.contains("好吃"));
+            drinkTag.setSelected(tags.contains("好喝"));
+            cultureTag.setSelected(tags.contains("文創"));
+            fashionTag.setSelected(tags.contains("穿搭時尚"));
+            otherTag.setSelected(tags.contains("其他"));
+
+            // 支援付款方式
+            Object mobile = vendor.get("support_mobile_payment");
+            mobilePay.setSelected(mobile instanceof Boolean && (Boolean) mobile);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "讀取商家資料失敗：" + e.getMessage());
+        }
     }
 }
