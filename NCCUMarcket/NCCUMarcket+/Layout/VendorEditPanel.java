9f69b7c3003b5d926cf1fc0fc6851dd69f2fbd2e
guaguaman1126 <113306006@g.nccu.edu.tw>
2025-05-13 10:13:51 +0800

    哈

--- NCCUMarcket/NCCUMarcket+/Layout/VendorEditPanel.java
+++ NCCUMarcket/NCCUMarcket+/Layout/VendorEditPanel.java
@@ -7,6 +7,11 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import javax.swing.*;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.lang.reflect.Type;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class VendorEditPanel extends JPanel {
 
@@ -155,5 +160,41 @@ public class VendorEditPanel extends JPanel {
         cultureTag.setSelected(false);
         fashionTag.setSelected(false);
         otherTag.setSelected(false);
+    }
+
+    public void refresh(String id) {
+        try {
+            URL url = new URL("https://nccu-market-default-rtdb.asia-southeast1.firebasedatabase.app/vendors/" + id + ".json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            InputStreamReader reader = new InputStreamReader(conn.getInputStream());
+            Gson gson = new Gson();
+            Type type = new TypeToken<Map<String, Object>>(){}.getType();
+            Map<String, Object> vendor = gson.fromJson(reader, type);
+            reader.close();
+
+            stallIdField.setText(id);
+            nameField.setText((String) vendor.get("name"));
+            contactField.setText((String) vendor.get("contact_info"));
+            promoArea.setText((String) vendor.get("description"));
+
+            // 標籤處理
+            String tags = (String) vendor.get("tags");
+            eatTag.setSelected(tags.contains("好吃"));
+            drinkTag.setSelected(tags.contains("好喝"));
+            cultureTag.setSelected(tags.contains("文創"));
+            fashionTag.setSelected(tags.contains("穿搭時尚"));
+            otherTag.setSelected(tags.contains("其他"));
+
+            // 支援付款方式
+            Object mobile = vendor.get("support_mobile_payment");
+            mobilePay.setSelected(mobile instanceof Boolean && (Boolean) mobile);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "讀取商家資料失敗：" + e.getMessage());
+        }
     }
 }
